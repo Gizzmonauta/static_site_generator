f1f33993ac60e41032c996171cb83f2351e0a413
Gizzmonauta <gizzmonauta@hotmail.com>
2026-01-11 16:39:36 -0500

    feat(leafnode): implement LeafNode class with to_html method and corresponding tests

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHtmlNode(unittest.TestCase):
 
@@ -92,6 +92,27 @@ class TestHtmlNode(unittest.TestCase):
         result = node.props_to_html()
         self.assertIn('data-info="a&b<c>d"', result)
         self.assertIn('empty=""', result)
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_a(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(node.to_html(), '<a href="https://www.google.com">Click me!</a>')
+
+    def test_leaf_to_html_no_tag(self):
+        node = LeafNode(None, "Hello, world!")
+        self.assertEqual(node.to_html(), "Hello, world!")
+
+    def test_leaf_to_html_no_value(self):
+        node = LeafNode("p", None)
+        with self.assertRaises(ValueError):
+            node.to_html()
+
+    def test_leaf_constructor_with_children(self):
+        with self.assertRaises(TypeError):
+            LeafNode(tag="p", value="text", children=[])
 
 if __name__ == "__main__":
     unittest.main()
